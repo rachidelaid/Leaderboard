b56bb3c74a2060f6216ac54d2b38c2f62632071a
rachidelaid <rachidelaid@gmail.com>
2022-01-25 16:08:38 +0100

    styling the list

--- src/render.js
+++ src/render.js
@@ -1,3 +1,7 @@
+const starSvg = `<svg viewBox="0 0 24 24">
+<path fill="currentColor" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+</svg>`;
+
 const renderList = (list) => {
   const container = document.querySelector('.list ul');
   container.innerHTML = '';
@@ -11,7 +15,10 @@ const renderList = (list) => {
     .sort((a, b) => b.score - a.score)
     .forEach((score, index) => {
       const li = document.createElement('li');
-      li.textContent = `${score.user}: ${score.score}`;
+      if (index < 3) {
+        li.innerHTML = starSvg;
+      }
+      li.innerHTML += `${score.user}: ${score.score}`;
       li.id = index;
 
       container.append(li);
